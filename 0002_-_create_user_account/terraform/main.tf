6a90e3f9b860d82689bff28ac79ff88012985f0f
Chris King-Parra <chris@kingparra.work>
2023-10-11 14:39:33 -0400

    Add TF to create user for Ricky

---
+++ 0002_-_create_user_account/terraform/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create the account
+resource "aws_iam_user" "rclemente" {
+  name = "rclemente"
+}
+
+# Create a power_users group
+resource "aws_iam_group" "power_users" {
+  name = "power_users"
+}
+
+# Attach the account to the the power_users group
+resource "aws_iam_group_membership" "power_users_membership" {
+  name = "tf-testing-group-membership"
+  users = [
+    aws_iam_user.rclemente.name,
+  ]
+  group = aws_iam_group.power_users.name
+}
+
+# Look up the PowerUserAccess AWS managed policy
+data "aws_iam_policy" "power_user_access" {
+    arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+# Attach that policy to the power_users group
+resource "aws_iam_group_policy_attachment" "test-attach" {
+  group      = aws_iam_group.power_users.name
+  policy_arn = aws_iam_policy.power_user_access.arnx
+}
+
+# Set up web console access for rclemente
+resource "aws_iam_user_login_profile" "rclemente" {
+  user = aws_iam_user.rclemente.name
+}
+
+output "rclemente_password" {
+  sensitive = true
+  value = aws_iam_user_login_profile.rclemente.password
+}
---
+++ 0002_-_create_user_account/worklog.rst
@@ -0,0 +1,6 @@
+* 13:59 PM EST - Moved ticket to in-progress. 
+* 14:02 PM EST - Created a branch. Wrote terraform provider and iam_user code.
+* 14:08 PM EST - Looked up how to create a iam group. Looked up how to retrive PowerUser managed policy arn. Looked up how to attach policy to group. Looked up how to allow console access to user.
+* 14:18 PM EST - Coding.
+* 14:24 PM EST - Wondering how I should send the password to Ricky -- If I had his PGP pubkey I could get the password output securely from terraform and send it directly on slack. For now I'll keep it in my password manager. Note that there is a password in the state file. Thankfully the backend is encrypted.
+* 14:37 - Commited and merged. Autoruns on spacelift.
