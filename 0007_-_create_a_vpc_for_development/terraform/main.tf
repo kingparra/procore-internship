2ca82534de28ac930a88f81324173877edb0e222
Chris King-Parra <chris@kingparra.work>
2023-10-17 14:19:06 -0400

    Ensure public subnets assign public ips 0007

--- 0007_-_create_a_vpc_for_development/terraform/main.tf
+++ 0007_-_create_a_vpc_for_development/terraform/main.tf
@@ -6,6 +6,7 @@ module "vpc" {
   cidr = var.vpc_cidr
   azs = var.azs
   private_subnets = var.private_subnets_cidrs
+  map_public_ip_on_launch = true # public subnets will assign public IP addrs
   public_subnets = var.public_subnets_cidrs
   enable_nat_gateway = false
 }
