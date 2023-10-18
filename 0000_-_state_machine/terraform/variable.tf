4e989ce4849745196c9aa1406d68bb080fb9d525
Chris King-Parra <chris@kingparra.work>
2023-10-18 13:37:33 -0400

    Create backend config for 0006

--- 0000_-_state_machine/terraform/variable.tf
+++ 0000_-_state_machine/terraform/variable.tf
@@ -2,6 +2,7 @@ variable "ticket_names" {
   type = list(string)
   description = "A list of ticket number and name combinations like 0000_-_ticket_title format. Will be used to generate a bucket."
   default = [ 
+    "0006_-_audit_solution_for_iam_changes",
     "0007_-_create_a_vpc_for_development",
     "0008_-_create_a_vpc_for_production",
   ]
