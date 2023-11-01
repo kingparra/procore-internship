869f486b27d1d542d33359c218a9f0dbfa15a8ac
Chris King-Parra <chris@kingparra.work>
2023-11-01 12:57:39 -0400

    Add service linked role for CodeDeploy 0011

--- 0011_-_improve_the_deployment_of_the_procore_plus_website/terraform/iam.tf
+++ 0011_-_improve_the_deployment_of_the_procore_plus_website/terraform/iam.tf
@@ -1,10 +1,10 @@
 resource "aws_iam_instance_profile" "deploy" {
   name = "deploy-procore-website"
-  role = aws_iam_role.codecommit.name
+  role = aws_iam_role.deploy.name
 }
 
-resource "aws_iam_role" "codecommit" {
-  name = "codecommit-procore-website"
+resource "aws_iam_role" "deploy" {
+  name = "deploy-procore-website"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -20,8 +20,8 @@ resource "aws_iam_role" "codecommit" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codecommit" {
-  role       = aws_iam_role.codecommit.name
+resource "aws_iam_role_policy_attachment" "deploy" {
+  role       = aws_iam_role.deploy.name
   policy_arn = data.aws_iam_policy.codecommit.arn
 }
 
@@ -29,6 +29,29 @@ data "aws_iam_policy" "codecommit" {
   arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
 }
 
-# resource "aws_iam_role" "codedeploy" {
-  
-# }
+# Add service linked role for CodeDeploy
+resource "aws_iam_role" "codedeploy" {
+  name = "AWSCodeDeployRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+data "aws_iam_policy" "codedeploy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy" {
+  role = aws_iam_role.codedeploy.name
+  policy_arn = data.aws_iam_policy.codedeploy.arn
+}
