5d16709de657a9de3ff4e91870caf44e02ef98cb
Chris King-Parra <chris@kingparra.work>
2023-11-05 13:15:38 -0500

    Rename variables in parsexml

--- 0009_-_create_a_vpn_connection/bash/parsexml.py
+++ 0009_-_create_a_vpn_connection/bash/parsexml.py
@@ -4,24 +4,21 @@ import textwrap
 
 
 def parse_xml(xml_data):
-    # Parse the XML data
     root = ET.fromstring(xml_data)
-
-    # Extract the necessary information
-    customer_gateway = root.find('.//customer_gateway/tunnel_outside_address/ip_address').text
-    vpn_gateway = root.find('.//vpn_gateway/tunnel_outside_address/ip_address').text
+    cgw = root.find('.//customer_gateway/tunnel_outside_address/ip_address').text
+    vgw = root.find('.//vpn_gateway/tunnel_outside_address/ip_address').text
     psk = root.find('.//ike/pre_shared_key').text
+    return (cgw, vgw, psk)
 
-    return (customer_gateway, vpn_gateway, psk)
 
-def generate_tun1(customer_gateway, vpn_gateway, onprem_cidr, remote_cidr):
+def generate_tun1(cgw, vgw, onprem_cidr, remote_cidr):
     openswan_config = textwrap.dedent(f"""\
         conn Tunnel1
           authby=secret
           auto=start
           left=%defaultroute
-          leftid={customer_gateway}
-          right={vpn_gateway}
+          leftid={cgw}
+          right={vgw}
           type=tunnel
           ikelifetime=8h
           keylife=1h
@@ -39,12 +36,14 @@ def generate_tun1(customer_gateway, vpn_gateway, onprem_cidr, remote_cidr):
     return openswan_config
 
 
-def generate_secrets(customer_gateway, vpn_gateway, psk):
-    return f"{customer_gateway} {vpn_gateway}: PSK \"{psk}\""
+
+def generate_secrets(cgw, vgw, psk):
+    return f"{cgw} {vgw}: PSK \"{psk}\""
 
 
-# Print the generated Openswan configuration
+
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Generate openswan config file from xml")
     parser.add_argument("xml_file", help="XML file containing VPN connection data")
     parser.add_argument("onprem_cidr", help="CIDR of onprem VPC")
@@ -57,12 +56,10 @@ if __name__ == "__main__":
         tun1_config = generate_tun1(cgw, vgw, args.onprem_cidr, args.remote_cidr)
         tun1_secrets = generate_secrets(cgw, vgw, psk)
 
-    # Write the Tunnel1 configuration to 'tun1.conf'
         with open('tun1.conf', 'w') as config_file:
             config_file.write(tun1_config)
-        print("Tunnel1 Configuration has been written to 'tun1.conf'")
+            print("Tunnel1 Configuration has been written to 'tun1.conf'")
 
-        # Write the Tunnel1 Secrets configuration to 'tun1.secrets'
         with open('tun1.secrets', 'w') as secrets_file:
             secrets_file.write(tun1_secrets)
-        print("Tunnel1 Secrets Configuration has been written to 'tun1.secrets'")
+            print("Tunnel1 Secrets Configuration has been written to 'tun1.secrets'")
