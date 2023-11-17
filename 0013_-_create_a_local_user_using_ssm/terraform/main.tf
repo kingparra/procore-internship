1fefee744985f9f5b96caabde5519fd7443f240b
Chris King-Parra <chris@kingparra.work>
2023-11-17 12:47:45 -0500

    Remove resource group

--- 0013_-_create_a_local_user_using_ssm/terraform/main.tf
+++ 0013_-_create_a_local_user_using_ssm/terraform/main.tf
@@ -2,5 +2,5 @@ resource "aws_ssm_document" "create_linux_user" {
   name = "create_linux_user"
   document_type = "Command"
   document_format = "YAML"
-  content = file("${module.path}../ssm_doc/doc.yaml")
+  content = file("${path.module}/../ssm_doc/doc.yaml")
 }
