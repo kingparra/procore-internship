0c86856eed6ccb1adb5023ce2471a0a7f7822b52
Chris King-Parra <chris@kingparra.work>
2023-12-04 17:15:18 -0500

    Rename backend bucket for 0017 to 0018

--- 0000_-_state_machine/terraform/backends/0018_-_test_the_deployment_of_a_splunk_enterprise_server_and_universal_forwarder_agents_backend.tf
+++ 0000_-_state_machine/terraform/backends/0018_-_test_the_deployment_of_a_splunk_enterprise_server_and_universal_forwarder_agents_backend.tf
@@ -3,9 +3,9 @@ terraform {
 
   backend "s3" {
     region         = "us-east-1"
-    bucket         = "0017-testthedeploymentofasplunkenterpriseserverandunivers-0438b"
+    bucket         = "0018-testthedeploymentofasplunkenterpriseserverandunivers-af71e"
     key            = "terraform.tfstate"
-    dynamodb_table = "0017-testthedeploymentofasplunkenterpriseserveranduniversalforwarderagents-state-lock"
+    dynamodb_table = "0018-testthedeploymentofasplunkenterpriseserveranduniversalforwarderagents-state-lock"
     profile        = ""
     role_arn       = ""
     encrypt        = "true"
